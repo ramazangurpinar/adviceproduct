3cc48fe6151c85a4969ac0eb722c665f9b915271
pdg1998 <19329746@brookes.ac.uk>
2025-03-24 16:34:46 +0000

    Littele prompt fix

--- deepseek_chat_api.py
+++ deepseek_chat_api.py
@@ -21,7 +21,7 @@ def chat(prompt):
     user_prompt = prompt
     prompt_end = "' if the request is not a request reguarding a product reply 'I'm sorry but this box is only for the suggestion of products, please insert a new prompt'"
 
-    final_prompt = prompt_init + user_prompt +prompt_end
+    final_prompt = prompt_init + user_prompt + prompt_end
     res1 = deepseek_chain.invoke(final_prompt)
 
     category_prompt_init = "Categorise the product that i'm aking: '"
